ba42690fa6b84af1b04d7a56f5013cb733467f76
CSa1t <114924202+CSa1t@users.noreply.github.com>
2022-10-16 11:54:15 +1000

    Update __init__.py

--- projectfile/website/__init__.py
+++ projectfile/website/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
+db=SQLAlchemy()
 
 def create_app():
     app=Flask(__name__)
-    
-    #add Blueprints
+
+    bootstrap = Bootstrap(app)
+
+    #Secret key
+    app.secret_key='somerandomvalue'
+
+    #Blueprints
     from . import views
     app.register_blueprint(views.mainbp)
     from . import markets
