6f1b5bc91de929a6fb042619b7df4061f0bab80c
CSa1t <114924202+CSa1t@users.noreply.github.com>
2022-10-16 11:49:50 +1000

    Update models.py

---
+++ projectfile/website/models.py
@@ -0,0 +1,19 @@
+from . import db
+from datetime import datetime
+
+
+class Market(db.Model):
+    __tablename__ = 'Markets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    description = db.Column(db.String(200))
+    image = db.Column(db.String(400))
+    currency = db.Column(db.String(3))
+    # ... Create the Comments db.relationship
+	# relation to call destination.comments and comment.destination
+    comments = db.relationship('Comment', backref='markets')
+
+    
+	
+    def __repr__(self): #string print method
+        return "<Name: {}>".format(self.name)
